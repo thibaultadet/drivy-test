3d09cbfa8d214e7918bd733d3b16a0332fb9b944
Thibault Adet <thibault.adet@gmail.com>
2020-04-22 17:24:19 +0200

    level 1

---
+++ backend/level1/main.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'pry-byebug'
+require 'json'
+require 'date'
+
+# 1- get the data from the input
+filepath = './data/input.json'
+inputs = JSON.parse(File.read(filepath))
+cars = inputs['cars']
+rentals = inputs['rentals']
+
+# initialize the output
+outputs = { rentals: [] }
+
+# 2- calculate price
+rentals.each do |rental|
+  # select the rental car
+  car_rented = cars.select { |car| car['id'] == rental['car_id'] }.first
+  # calculate the rental time (+ 1 to add include the first day)
+  time = (Date.parse(rental['end_date']) - Date.parse(rental['start_date']) + 1)
+  # calculate price for rental time
+  time_price = time.to_i * car_rented['price_per_day']
+  # calculate price for rental distance
+  distance_price = rental['distance'] * car_rented['price_per_km']
+  # add the id and price of rental in outputs
+  outputs[:rentals] << {
+    id: rental['id'],
+    price: time_price + distance_price
+  }
+end
+
+# 3- store the rentals data with price
+File.open('./data/output.json', 'wb') do |file|
+  file.write(JSON.generate(outputs))
+end
